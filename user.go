d8b20e5c460c6013dfba741d0ae50835157c84e0
Abdul Dakkak <abduld@wolfram.com>
2017-03-12 09:08:28 +0000

    switch from toml to yaml

--- user.go
+++ user.go
@@ -1,13 +1,13 @@
 package model
 
 type User struct {
-	Base
-	Firstname string `json:"firstname" toml:"firstname,omitempty"`
-	Lastname  string `json:"lastname" toml:"lastname,omitempty"`
-	Username  string `json:"username" toml:"username"`
-	Email     string `json:"email" toml:"email"`
-	AccessKey string `json:"access_key" toml:"access_key"`
-	SecretKey string `json:"secret_key" toml:"secret_key"`
-	Password  string `json:"password" toml:"-"`
+	Base      `toml:"-" yaml:"-"`
+	Firstname string `json:"firstname" yaml:"firstname,omitempty" toml:"firstname,omitempty"`
+	Lastname  string `json:"lastname" yaml:"lastname,omitempty" toml:"lastname,omitempty"`
+	Username  string `json:"username" yaml:"username" toml:"username"`
+	Email     string `json:"email" yaml:"email" toml:"email"`
+	AccessKey string `json:"access_key" yaml:"access_key" toml:"access_key"`
+	SecretKey string `json:"secret_key" yaml:"secret_key" toml:"secret_key"`
+	Password  string `json:"password" yaml:"password,omitempty" toml:"-"`
 	// Team      *Team  `json:"team" gorm:"ForeignKey:TeamID;AssociationForeignKey:Refer" toml:"-"`
 }
