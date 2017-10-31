90a783e4defa08ed20ced874a5a33bfcef6d0715
Carl Pearson <pearson@illinois.edu>
2017-10-31 13:10:00 -0500

    some work on Fa2017Ece408Ranking

--- base.go
+++ base.go
@@ -1,10 +1,14 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+)
 
 type Base struct {
-	ID        string     `json:"id" gorm:"primary_key" toml:"id,omitempty"`
-	CreatedAt time.Time  `json:"created_at" toml:"created_at,omitempty"`
-	UpdatedAt time.Time  `json:"updated_at" toml:"updated_at,omitempty"`
-	DeletedAt *time.Time `json:"deleted_at" sql:"index" toml:"deleted_at,omitempty"`
+	ID        bson.ObjectId `json:"id" bson:"_id,omitempty" gorm:"primary_key" toml:"id,omitempty"`
+	CreatedAt time.Time     `json:"created_at" toml:"created_at,omitempty"`
+	UpdatedAt time.Time     `json:"updated_at" toml:"updated_at,omitempty"`
+	DeletedAt *time.Time    `json:"deleted_at" sql:"index" toml:"deleted_at,omitempty"`
 }
--- ranking.go
+++ ranking.go
@@ -1,10 +1,37 @@
 package model
 
-import "time"
+import (
+	"strings"
+	"time"
 
-// Ranking holds info used to track project team rankings
+	"github.com/docker/docker/pkg/namesgenerator"
+)
+
+// Ranking holds info used to track team rankings
 type Ranking struct {
-	OpFullRuntime time.Duration
-	S3Key         string
-	ProjectURL    string
+	Base
+	Username   string
+	Teamname   string
+	ProjectURL string // where the file was uploaded
 }
+
+// Anonymize produces an anonymous Ranking
+func (r Ranking) Anonymize() Ranking {
+	ret := r
+	teamname := strings.Join(strings.Split(namesgenerator.GetRandomName(0), "_"), " ")
+	ret.Teamname = strings.Title(teamname)
+	ret.ProjectURL = "--"
+	return ret
+}
+
+// Fa2017Ece408Ranking holds fields specific to ECE408
+type Fa2017Ece408Ranking struct {
+	Ranking
+	BatchSize         int
+	Correctness       float64
+	OpRuntime         time.Duration // runtime reported by the layer
+	UserFullRuntime   time.Duration // runtime captured using time
+	SystemFullRuntime time.Duration // full runtime captures using time
+}
+
+type Fa2017Ece408Rankings []Fa2017Ece408Ranking
