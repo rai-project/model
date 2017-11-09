f79217bf0147b600c375437b08e4713a9fb8c773
Carl Pearson <pearson@illinois.edu>
2017-11-09 16:45:45 -0600

    Support multiple inferences per job

--- ranking.go
+++ ranking.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"hash/fnv"
+	"math"
 	"strconv"
 	"time"
 
@@ -9,6 +10,16 @@ import (
 	"github.com/rai-project/database"
 	"github.com/rai-project/database/mongodb"
 )
+
+//Inference represents a forward run of the network
+type Inference struct {
+	Model              string
+	Correctness        float64
+	OpRuntime          time.Duration // runtime reported by the layer
+	UserFullRuntime    time.Duration // user from /usr/bin/time
+	SystemFullRuntime  time.Duration // system from /usr/bin/time
+	ElapsedFullRuntime time.Duration // elapsed from /usr/bin/time
+}
 
 // Ranking holds info used to track team rankings
 type Ranking struct {
@@ -20,15 +31,32 @@ type Ranking struct {
 	SubmissionTag string `bson:"submission_tag"` // more info about the submission
 }
 
-// Fa2017Ece408Ranking holds fields specific to ECE408
-type Fa2017Ece408Ranking struct {
-	Ranking            `bson:",inline"`
-	Model              string
-	Correctness        float64
-	OpRuntime          time.Duration // runtime reported by the layer
-	UserFullRuntime    time.Duration // user from /usr/bin/time
-	SystemFullRuntime  time.Duration // system from /usr/bin/time
-	ElapsedFullRuntime time.Duration // elapsed from /usr/bin/time
+// Fa2017Ece408Job holds fields specific to ECE408
+type Fa2017Ece408Job struct {
+	Ranking    `bson:",inline"`
+	Inferences []Inference
+}
+
+func (j *Fa2017Ece408Job) MinOpRuntime() time.Duration {
+	minSeen := int64(math.MaxInt64)
+
+	for _, i := range j.Inferences {
+		if int64(i.OpRuntime) < minSeen {
+			minSeen = int64(i.OpRuntime)
+		}
+	}
+	return time.Duration(minSeen)
+}
+
+func (j *Fa2017Ece408Job) StartNewInference() {
+	j.Inferences = append(j.Inferences, Inference{})
+}
+
+func (j *Fa2017Ece408Job) CurrentInference() *Inference {
+	if len(j.Inferences) == 0 {
+		j.StartNewInference()
+	}
+	return &j.Inferences[len(j.Inferences)-1]
 }
 
 func anonymizeString(s string) string {
@@ -37,8 +65,8 @@ func anonymizeString(s string) string {
 	return strconv.FormatUint(uint64(h.Sum32()), 10)
 }
 
-// Anonymize produces an anonymous Fa2017Ece408Ranking
-func (r Fa2017Ece408Ranking) Anonymize() Fa2017Ece408Ranking {
+// Anonymize produces an anonymous Fa2017Ece408Job
+func (r Fa2017Ece408Job) Anonymize() Fa2017Ece408Job {
 	h := fnv.New32a()
 	h.Write([]byte(r.Teamname + ":::" + config.App.Secret))
 	ret := r
@@ -48,34 +76,34 @@ func (r Fa2017Ece408Ranking) Anonymize() Fa2017Ece408Ranking {
 	return ret
 }
 
-func (Fa2017Ece408Ranking) TableName() string {
+func (Fa2017Ece408Job) TableName() string {
 	return "rankings"
 }
 
-type Fa2017Ece408RankingCollection struct {
+type Fa2017Ece408JobCollection struct {
 	*mongodb.MongoTable
 }
 
-func NewFa2017Ece408RankingCollection(db database.Database) (*Fa2017Ece408RankingCollection, error) {
-	tbl, err := mongodb.NewTable(db, Fa2017Ece408Ranking{}.TableName())
+func NewFa2017Ece408JobCollection(db database.Database) (*Fa2017Ece408JobCollection, error) {
+	tbl, err := mongodb.NewTable(db, Fa2017Ece408Job{}.TableName())
 	if err != nil {
 		return nil, err
 	}
 	tbl.Create(nil)
 
-	return &Fa2017Ece408RankingCollection{
+	return &Fa2017Ece408JobCollection{
 		MongoTable: tbl.(*mongodb.MongoTable),
 	}, nil
 }
 
-func (m *Fa2017Ece408RankingCollection) Close() error {
+func (m *Fa2017Ece408JobCollection) Close() error {
 	return nil
 }
 
-type Fa2017Ece408Rankings []Fa2017Ece408Ranking
+type Fa2017Ece408Jobs []Fa2017Ece408Job
 
-func KeepFirstTeam(rs Fa2017Ece408Rankings) Fa2017Ece408Rankings {
-	res := Fa2017Ece408Rankings{}
+func KeepFirstTeam(rs Fa2017Ece408Jobs) Fa2017Ece408Jobs {
+	res := Fa2017Ece408Jobs{}
 
 	seen := map[string]interface{}{}
 	for _, r := range rs {
@@ -88,9 +116,8 @@ func KeepFirstTeam(rs Fa2017Ece408Rankings) Fa2017Ece408Rankings {
 	return res
 }
 
-// ByOpRuntime allows sorting Fa2017Ece408Rankings by OpRuntime
-type ByOpRuntime []Fa2017Ece408Ranking
+type ByMinOpRuntime []Fa2017Ece408Job
 
-func (r ByOpRuntime) Len() int           { return len(r) }
-func (r ByOpRuntime) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
-func (r ByOpRuntime) Less(i, j int) bool { return r[i].OpRuntime < r[j].OpRuntime }
+func (r ByMinOpRuntime) Len() int           { return len(r) }
+func (r ByMinOpRuntime) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+func (r ByMinOpRuntime) Less(i, j int) bool { return r[i].MinOpRuntime() < r[j].MinOpRuntime() }
