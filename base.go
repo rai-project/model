c8c04d11c067fb6c722238f1888baab66838f3c9
Abdul Dakkak <abduld@wolfram.com>
2018-10-13 17:46:39 -0500

    use bson as id

--- base.go
+++ base.go
@@ -2,12 +2,14 @@ package model
 
 import (
 	"time"
+
+	"gopkg.in/mgo.v2/bson"
 )
 
 // easyjson:json
 type Base struct {
-	ID        string     `json:"id" gorm:"primary_key" toml:"id,omitempty" validate:"required"`
-	CreatedAt time.Time  `json:"created_at" toml:"created_at,omitempty" validate:"required"`
-	UpdatedAt time.Time  `json:"updated_at" toml:"updated_at,omitempty" validate:"required"`
-	DeletedAt *time.Time `json:"deleted_at" sql:"index" toml:"deleted_at,omitempty"`
+	ID        bson.ObjectId `json:"id" bson:"_id" gorm:"primary_key" toml:"id,omitempty" validate:"required"`
+	CreatedAt time.Time     `json:"created_at" toml:"created_at,omitempty" validate:"required"`
+	UpdatedAt time.Time     `json:"updated_at" toml:"updated_at,omitempty" validate:"required"`
+	DeletedAt *time.Time    `json:"deleted_at" sql:"index" toml:"deleted_at,omitempty"`
 }
